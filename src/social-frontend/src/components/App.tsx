694756c2c4ec2bf4a82e166ebb963898727c029e
Dawid Michalak <dawid.michalak0905@gmail.com>
2022-08-13 12:12:09 +0200

    #2 Add project files for social frontend

---
+++ src/social-frontend/src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="app">
+      <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
