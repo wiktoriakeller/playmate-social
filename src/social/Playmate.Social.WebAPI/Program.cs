94bee2a42cd54e252b3acd511bed3e3e0ad50237
Dawid Michalak <dawid.michalak0905@gmail.com>
2022-10-12 20:41:09 +0200

    Add cors, disable https redirection

--- src/social/Playmate.Social.WebAPI/Program.cs
+++ src/social/Playmate.Social.WebAPI/Program.cs
@@ -6,14 +6,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("cors-policy", builder => builder.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors("cors-policy");
 app.UseSwagger();
 
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
