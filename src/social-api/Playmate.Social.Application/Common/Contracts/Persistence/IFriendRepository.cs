55a722c0dc1307c6deafb23d7c6473278b48622b
DawidMichalak <dawid.michalak0905@gmail.com>
2022-10-26 23:40:44 +0200

    Add friend repository

---
+++ src/social-api/Playmate.Social.Application/Common/Contracts/Persistence/IFriendRepository.cs
@@ -0,0 +1,8 @@
+﻿using Playmate.Social.Domain.Entities;
+
+namespace Playmate.Social.Application.Common.Contracts.Persistence;
+public interface IFriendRepository : IRepository<Friend>
+{
+    Task<Friend?> GetFriend(User user, Guid friendId);
+    Task<IEnumerable<User>> GetFriends(User user);
+}
---
+++ src/social-api/Playmate.Social.Infrastructure/Persistence/Repositories/FriendRepository.cs
@@ -0,0 +1,34 @@
+﻿using Playmate.Social.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Playmate.Social.Application.Common.Contracts.Persistence;
+using MediatR;
+
+namespace Playmate.Social.Infrastructure.Persistence.Repositories;
+public class FriendRepository : BaseRepository<Friend>, IFriendRepository
+{
+    public FriendRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<User>> GetFriends(User user)
+    {
+        var friends = _dbContext.Set<Friend>()
+            .Where(f => f.RequesterId == user.Id)
+            .Include(f => f.Addressee)
+            .Select(f => f.Addressee);
+
+        var friends2 = _dbContext.Set<Friend>()
+            .Where(f => f.AddresseeId == user.Id)
+            .Include(f => f.Requester)
+            .Select(f => f.Requester).Concat(friends);
+
+        return await friends2.ToListAsync();
+    }
+
+    public async Task<Friend?> GetFriend(User user, Guid friendId)
+    {
+        var friend = await _dbContext.Set<Friend>().Where(f => f.AddresseeId == user.Id && f.RequesterId == friendId ||
+        f.RequesterId == user.Id && f.AddresseeId == friendId).FirstOrDefaultAsync();
+        return friend;
+    }
+}
