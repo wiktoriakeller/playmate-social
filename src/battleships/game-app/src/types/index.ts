a4fb9a4ecfc3e73de4d1d500f2c29f2203ec86d9
jedrzejSmok <jedrzejsmok55@gmail.com>
2022-11-10 15:06:56 +0100

    feature/18-battleships delete again game-app

--- src/battleships/game-app/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './SendMessageBoxProps';
-export * from './ReceiveMessageBoxProps';
-export * from './WebSocketServiceProps';
-export * from './JsonValue';
-export * from './WebSocketOptions';
-export * from './WebSocketMessage';
