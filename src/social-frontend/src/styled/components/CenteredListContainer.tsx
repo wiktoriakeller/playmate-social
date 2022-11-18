0378b49527b39941d652fc4fb045bb2bc964da77
DawidMichalak <dawid.michalak0905@gmail.com>
2022-11-18 11:33:16 +0100

    Add friends dialog

---
+++ src/social-frontend/src/styled/components/CenteredListContainer.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const CenteredListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
+`;
