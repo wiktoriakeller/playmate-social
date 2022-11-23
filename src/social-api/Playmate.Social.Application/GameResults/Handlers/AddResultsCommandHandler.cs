ee92e08e7f539575cb438e77d25fded096778e51
DawidMichalak <dawid.michalak0905@gmail.com>
2022-11-23 22:49:57 +0100

    Add time provider

--- src/social-api/Playmate.Social.Application/GameResults/Handlers/AddResultsCommandHandler.cs
+++ src/social-api/Playmate.Social.Application/GameResults/Handlers/AddResultsCommandHandler.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using Playmate.Social.Application.Common;
 using Playmate.Social.Application.Common.BaseResponse;
+using Playmate.Social.Application.Common.Contracts;
 using Playmate.Social.Application.Common.Contracts.Persistence;
-using Playmate.Social.Application.Friends.Responses;
 using Playmate.Social.Application.GameResults.Commands;
 using Playmate.Social.Application.GameResults.Responses;
 using Playmate.Social.Domain.Entities;
@@ -12,19 +12,21 @@ namespace Playmate.Social.Application.GameResults.Handlers;
 public class AddResultsCommandHandler : IHandlerWrapper<AddResultsCommand, AddResultsResponse>
 {
     private readonly IRepository<GameResult> _resultsRepository;
+    private readonly IDateTimeProvider _dateTimeProvider;
     private readonly IMapper _mapper;
 
-    public AddResultsCommandHandler(IRepository<GameResult> resultsRepository, IMapper mapper)
+    public AddResultsCommandHandler(IRepository<GameResult> resultsRepository, IMapper mapper, IDateTimeProvider dateTimeProvider)
     {
         _resultsRepository = resultsRepository;
         _mapper = mapper;
+        _dateTimeProvider = dateTimeProvider;
     }
 
     public async Task<Response<AddResultsResponse>> Handle(AddResultsCommand request, CancellationToken cancellationToken)
     {
         var mappedResult = _mapper.Map<GameResult>(request);
 
-        mappedResult.Date = DateTime.Now;
+        mappedResult.Date = _dateTimeProvider.CurrentTime;
 
         var created = await _resultsRepository.AddAsync(mappedResult);
         return ResponseResult.Created(new AddResultsResponse(created.Id));
