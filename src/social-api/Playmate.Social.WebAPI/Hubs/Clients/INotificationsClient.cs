619e0131bb81fa0fd1d095e0759f83dd2f6c639e
DawidMichalak <dawid.michalak0905@gmail.com>
2022-12-07 16:11:46 +0100

    Add friend notifications to hub

--- src/social-api/Playmate.Social.WebAPI/Hubs/Clients/INotificationsClient.cs
+++ src/social-api/Playmate.Social.WebAPI/Hubs/Clients/INotificationsClient.cs
@@ -1,8 +1,11 @@
-﻿using Playmate.Social.WebAPI.HubRequests;
+﻿using Playmate.Social.Application.Friends.Dtos;
+using Playmate.Social.WebAPI.HubRequests;
 
 namespace Playmate.Social.WebAPI.Hubs.Clients;
 
 public interface INotificationsClient
 {
     Task ReceiveChatMessage(SendChatMessageRequest request);
+    Task ReceiveFriendsRequest(FriendRequestDto request);
+    Task ReceiveFriendsRequestConfirmation();
 }
--- src/social-api/Playmate.Social.WebAPI/Hubs/NotificationsHub.cs
+++ src/social-api/Playmate.Social.WebAPI/Hubs/NotificationsHub.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Playmate.Social.Application.Friends.Commands;
+using Playmate.Social.WebAPI.ApiRequests.Friends;
 using Playmate.Social.WebAPI.HubRequests;
 using Playmate.Social.WebAPI.Hubs.Clients;
 
@@ -9,9 +13,39 @@ namespace Playmate.Social.WebAPI.Hubs;
 public class NotificationsHub : Hub<INotificationsClient>
 {
     public static string HubPath = "/hubs/notifications";
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public NotificationsHub(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
 
     public async Task SendChatMessage(SendChatMessageRequest request)
     {
         await Clients.User(request.ReceiverId).ReceiveChatMessage(request);
+    }
+
+    public async Task AnswerFriendRequest(AnswerRequest answerRequest)
+    {
+        var command = _mapper.Map<AnswerFriendRequestCommand>(answerRequest);
+        var response = await _mediator.Send(command);
+        
+        if (response.Succeeded)
+        {
+            await Clients.User(answerRequest.RequesterId.ToString()).ReceiveFriendsRequestConfirmation();
+        }
+    }
+
+    public async Task SendFriendRequest(AddFriendRequest request)
+    {
+        var command = _mapper.Map<AddFriendRequestCommand>(request);
+        var response = await _mediator.Send(command);
+
+        if (response.Succeeded)
+        {
+            await Clients.User(request.ReceiverId.ToString()).ReceiveFriendsRequest(response.Data.Request);
+        }
     }
 }
