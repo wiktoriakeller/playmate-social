201174881a8832255776c4f681fecaa29585e9a7
jedrzejsmok <jedrzejsmok55@gmail.com>
2022-12-07 17:54:04 +0100

    feature/81-emoji-picker-for-chat fix theme

--- src/social-frontend/src/components/chat/ChatEmojiPicker.tsx
+++ src/social-frontend/src/components/chat/ChatEmojiPicker.tsx
@@ -1,25 +1,29 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import { Tooltip, ClickAwayListener, Box } from "@mui/material";
 import { StyledIconButton } from "../../styled/components/mui/StyledIconButton";
 import { EmojiPickerContainer } from "../../styled/components/chat/EmojiPickerContainer";
+import { selectTheme } from "../../slices/themeSlice";
+import { useAppSelector } from "../../app/hooks";
 import Popper from "@mui/material/Popper";
 import EmojiPicker from "emoji-picker-react";
-import { EmojiClickData } from "emoji-picker-react";
+import { EmojiClickData, EmojiStyle, Theme } from "emoji-picker-react";
 
 export interface IChatEmojiPicker {
   onEmojiClick: (e: EmojiClickData) => void;
   chatInputTextRef: React.MutableRefObject<HTMLInputElement>;
 }
 const ChatEmojiPicker = (props: IChatEmojiPicker) => {
+  const themeMode = useAppSelector(selectTheme);
   const [open, setOpen] = useState(false);
-  const handleClick = () => {
-    setOpen(true);
-  };
 
-  const handleClose = () => {
+  const handleClick = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleClose = useCallback(() => {
     setOpen(false);
-  };
+  }, []);
   return (
     <Tooltip title="Choose an emoji">
       <div>
@@ -34,7 +38,11 @@ const ChatEmojiPicker = (props: IChatEmojiPicker) => {
           <ClickAwayListener onClickAway={handleClose}>
             <Box>
               <EmojiPickerContainer>
-                <EmojiPicker onEmojiClick={props.onEmojiClick} />
+                <EmojiPicker
+                  onEmojiClick={props.onEmojiClick}
+                  emojiStyle={EmojiStyle.GOOGLE}
+                  theme={themeMode.theme as Theme}
+                />
               </EmojiPickerContainer>
             </Box>
           </ClickAwayListener>
