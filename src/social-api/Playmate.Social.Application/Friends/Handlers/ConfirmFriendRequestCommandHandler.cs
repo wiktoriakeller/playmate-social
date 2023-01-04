dcdd397f0a8392fe7e0ae72086c9985ee922250d
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2023-01-04 10:54:55 +0100

    Fixes for deploy

--- src/social-api/Playmate.Social.Application/Friends/Handlers/ConfirmFriendRequestCommandHandler.cs
+++ src/social-api/Playmate.Social.Application/Friends/Handlers/ConfirmFriendRequestCommandHandler.cs
@@ -15,17 +15,20 @@ public class ConfirmFriendRequestCommandHandler : IHandlerWrapper<ConfirmFriendR
     private readonly IFriendRequestsRepository _friendsRequestsRepository;
     private readonly IFriendsRepository _friendsRepository;
     private readonly ICurrentUserService _currentUserService;
+    private readonly IUsersRepository _usersRepository;
     private readonly IMapper _mapper;
 
     public ConfirmFriendRequestCommandHandler(
         IFriendRequestsRepository friendsRequestsRepository,
         IFriendsRepository friendsRepository,
         ICurrentUserService currentUserService,
+        IUsersRepository usersRepository,
         IMapper mapper)
     {
         _friendsRequestsRepository = friendsRequestsRepository;
         _friendsRepository = friendsRepository;
         _currentUserService = currentUserService;
+        _usersRepository = usersRepository;
         _mapper = mapper;
     }
 
@@ -34,28 +37,39 @@ public class ConfirmFriendRequestCommandHandler : IHandlerWrapper<ConfirmFriendR
         var friendRequest = await _friendsRequestsRepository.GetByIdAsync(request.RequestId);
         if (friendRequest == null)
         {
-            return ResponseResult.NotFound<ConfirmFriendRequestResponse>("Friend request not found");
+            return ResponseResult.NotFound<ConfirmFriendRequestResponse>("Friend request was not found");
         }
 
         var currentUser = _currentUserService.CurrentUser;
-        if (friendRequest.AddresseeId != currentUser.Id)
+
+        if (friendRequest.AddresseeId != currentUser?.Id)
         {
-            ResponseResult.ValidationError<ConfirmFriendRequestResponse>("Current user is not the addressee of request");
+            ResponseResult.ValidationError<ConfirmFriendRequestResponse>("Current user is not the addressee of the request");
+        }
+
+        var requestAdresse = await _usersRepository.GetByIdAsync(_currentUserService.CurrentUser.Id);
+
+        if (requestAdresse is null)
+        {
+            ResponseResult.ValidationError<ConfirmFriendRequestResponse>("Request adresse was not found");
         }
 
         var response = new ConfirmFriendRequestResponse() { RequestAccepted = request.Accept };
 
         if (request.Accept)
         {
+            var friendsSince = DateTimeOffset.UtcNow;
             var friend = new Friend
             {
                 AddresseeId = friendRequest.AddresseeId,
                 RequesterId = friendRequest.RequesterId,
-                FriendsSince = DateTimeOffset.UtcNow,
+                FriendsSince = friendsSince,
             };
 
             await _friendsRepository.AddAsync(friend);
-            response.CreatedFriend = _mapper.Map<FriendDto>(currentUser);
+            response.CreatedFriend = _mapper.Map<FriendDto>(requestAdresse);
+            response.CreatedFriend.FriendsSince = friendsSince;
+            response.CreatedFriend.ProfilePictureUrl = requestAdresse?.ProfilePictureUrl ?? "";
         }
 
         await _friendsRequestsRepository.DeleteAsync(friendRequest);
